c568c5e592304a33893b4964e186784ac386dab9
wyq-sure <528562103@qq.com>
2022-04-11 21:50:53 +0800

    1

--- src/main/java/com/example/demo/service/DemoService.java
+++ src/main/java/com/example/demo/service/DemoService.java
@@ -8,7 +8,7 @@ public interface DemoService  {
      * @author wangyq
      * @date 2022/4/11 10:37
      * @Description 使用spring-retry注解实现重试
-     * @return
+     * @return 异步调用的返回值
      */
     public CompletableFuture<String> coreService() throws Exception;
 }
